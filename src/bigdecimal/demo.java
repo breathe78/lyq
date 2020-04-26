0846e3cc19348c47496ae3333c30048a9e6ab73d
liuyanqing <liuyanqing@kungeek.com>
2020-04-26 11:08:45 +0800

    bigdecimao 千分符

--- src/bigdecimal/demo.java
+++ src/bigdecimal/demo.java
@@ -6,6 +6,7 @@ import com.sun.deploy.util.StringUtils;
 
 import javax.sound.midi.Soundbank;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.*;
 
@@ -14,8 +15,22 @@ import java.util.*;
  */
 public class demo {
     public static void main(String[] args) {
+        BigDecimal a = new BigDecimal(75000);
+        BigDecimal b = new BigDecimal(74999);
+        BigDecimal c = new BigDecimal(75001);
 
+        //比较大小
+        System.out.println(new BigDecimal(75000).compareTo(new BigDecimal(75000)) > -1);
+        System.out.println(new BigDecimal(75000).compareTo(c) <0);
 
-        System.out.println(BigDecimal.ONE.divide(BigDecimal.ZERO));
+    }
+
+    /**
+     * bigdecimal 的千分符
+     */
+    public void qiFenFu() {
+        //千分符
+        DecimalFormat df1 = new DecimalFormat("###,###.##");
+        System.out.println(df1.format(1234567.126)); //"1,234,567.12"
     }
 }
