e4962ec53f9c7b8f1a6a18b253de48094d265a14
liuyanqing <liuyanqing@kungeek.com>
2020-05-26 10:41:02 +0800

    提交

---
+++ src/java8/FunctionTest.java
@@ -0,0 +1,39 @@
+package java8;
+
+import java.util.function.Function;
+
+public class FunctionTest {
+    public static void main(String[] args) {
+        FunctionTest test = new FunctionTest();
+        System.out.println(test.compute(3, item -> item + 1));
+        System.out.println(test.computeCompose(3, f1 -> f1 + 1, f2 -> f2 * f2));
+        System.out.println(test.computeAndThen(3, f1 -> f1 + 1, f2 -> f2 * f2));
+    }
+
+    /**
+     * Function实例
+     *
+     * @param a
+     * @param function
+     * @return
+     */
+    public int compute(int a, Function<Integer, Integer> function) {
+        return function.apply(a);
+    }
+
+    /**
+     * 先走Function2，再走Function1
+     *
+     * @param a
+     * @param f1
+     * @param f2
+     * @return
+     */
+    public int computeCompose(int a, Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+        return f1.compose(f2).apply(a);
+    }
+
+    public int computeAndThen(int a, Function<Integer, Integer> f1, Function<Integer, Integer> f2) {
+        return f1.andThen(f2).apply(a);
+    }
+}
--- src/java8/lambda_01.java
+++ src/java8/lambda_01.java
@@ -1,9 +1,7 @@
 package java8;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.concurrent.RunnableScheduledFuture;
 
 public class lambda_01 {
 
