8e2b1c75b46905e2241c2dd7a6c4847cc0bd8fc5
liuyanqing <liuyanqing@kungeek.com>
2020-06-22 17:01:25 +0800

    提交

--- src/java8/stream/StreamTest7.java
+++ src/java8/stream/StreamTest7.java
@@ -1,5 +1,6 @@
 package java8.stream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,7 +9,14 @@ public class StreamTest7 {
     public static void main(String[] args) {
         //将每个首字母变成大写
         List<String> list = Arrays.asList("hello", "world", "helloword");
-        list.stream().map(item -> item.substring(0, 1).toUpperCase().concat(item.substring(1))).collect(Collectors.toList()).forEach(System.out::println);
+        List<String> temp ;
+        //list.stream().map(item -> item.substring(0, 1).toUpperCase().concat(item.substring(1))).collect(Collectors.toList()).forEach(System.out::println);
+        temp = list.stream().filter(x -> x.endsWith("d")).collect(Collectors.toList());
+
+        List<String> myList = new ArrayList<>();
+        myList= temp;
+        myList.forEach(x-> System.out.println(x));
+
 
     }
 }
